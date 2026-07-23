f26a812ecb58c0dbf183b7e6d57a10ccd286e4ae
meowscan <meowscann@gmail.com>
2026-07-22 23:47:18 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'meow-scan-v1';
+const urlsToCache = [
+  '/miau-scan/',
+  '/miau-scan/index.html',
+  '/miau-scan/manifest.json'
+];
+
+// Instala o Service Worker e salva os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativa o Service Worker e limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepta as requisições: se estiver sem internet, pega do cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
